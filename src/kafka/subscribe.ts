9c46c6367d16c031fbec3901cf2b0211ae06db9a
Gilad Gur Andelman <gilad@loadmill.com>
2024-07-24 08:38:46 +0300

    🛒 Consume From ⏰ Timestamp

--- src/kafka/subscribe.ts
+++ src/kafka/subscribe.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'crypto';
 
-import { Kafka, KafkaMessage } from 'kafkajs';
+import { Consumer, Kafka, KafkaMessage, PartitionOffset } from 'kafkajs';
 
 import { APP_NAME } from '../constants';
 import { ConsumedMessage, SubscribeOptions, SubscribeParams } from '../types';
@@ -25,7 +25,8 @@ export const subscribe = async (
   const consumer = kafka.consumer({ groupId: id });
   const connection = addConnection(id, consumer, topic);
   await consumer.connect();
-  await consumer.subscribe({ fromBeginning: true, topic });
+  const partitions = await getPartitionsByTimestamp(kafka, topic);
+  await consumer.subscribe({ fromBeginning: false, topic });
   await consumer.run({
     eachMessage: async ({ message }) => {
       connection.messages.push(
@@ -33,7 +34,27 @@ export const subscribe = async (
       );
     },
   });
+  await assignPartitions(consumer, partitions, topic);
   return { id };
+};
+
+const getPartitionsByTimestamp = async (kafka: Kafka, topic: string): Promise<PartitionOffset[]> => {
+  const timestamp = get1MinuteAgoTimestamp();
+  const admin = kafka.admin();
+  await admin.connect();
+  const partitions = await admin.fetchTopicOffsetsByTimestamp(topic, timestamp);
+  await admin.disconnect();
+  return partitions;
+};
+
+const get1MinuteAgoTimestamp = () => Date.now() - 60 * 1000;
+
+const assignPartitions = async (consumer: Consumer, partitions: PartitionOffset[], topic: string) => {
+  await Promise.all(
+    partitions.map(({ partition, offset }) =>
+      consumer.seek({ offset, partition, topic }),
+    ),
+  );
 };
 
 const fromKafkaToConsumedMessage = async (message: KafkaMessage): Promise<ConsumedMessage> => {
