95cd5404bbd57575a9578cb70ea042d2b6012b63
Gilad Gur Andelman <gilad@loadmill.com>
2024-08-12 15:05:47 +0300

    getMessagesFromRedis surface errors

--- src/kafka/subscribers/get-messages-from-redis.ts
+++ src/kafka/subscribers/get-messages-from-redis.ts
@@ -5,13 +5,8 @@ import { toMessagesKey } from './redis-keys';
 
 export const getMessagesFromRedis = async (subscriberId: string): Promise<ConsumedMessage[]> => {
   const serializedMessages = await getRedisClient().lRange(toMessagesKey(subscriberId), 0, -1);
-  try {
-    const messages = serializedMessages.map(
-      (message: string) => JSON.parse(message) as ConsumedMessage,
-    );
-    return messages;
-  } catch (e) {
-    return [];
-  }
-  return [];
+  const messages = serializedMessages.map(
+    (message: string) => JSON.parse(message) as ConsumedMessage,
+  );
+  return messages;
 };
