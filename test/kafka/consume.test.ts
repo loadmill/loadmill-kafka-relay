8a14263877fdcd1eafb11babb1d39a86342aa394
Raz Darzi <raz.darzi@riskified.com>
2025-03-25 14:20:37 +0200

    add tests

--- test/kafka/consume.test.ts
+++ test/kafka/consume.test.ts
@@ -25,4 +25,47 @@ describe('filterMessages', () => {
     expect(res).toEqual([message2]);
   });
 
+  it('should filter a message by a value only', async () => {
+    const regexFilter = 'request-id-2';
+    const message1 = { headers: {
+        'key1': 'value1',
+
+      },
+      timestamp: 'stamp',
+      value: 'request-id-2',
+    } as ConsumedMessage;
+
+    const message2 = { headers: {
+        'key1': 'value1', 'x-internal-request-id': 'request-id-1',
+      },
+      timestamp: 'stamp2',
+      value: 'message2',
+    } as ConsumedMessage;
+
+    const consumedMessages = [message1, message2];
+    const res = filterMessages(consumedMessages, regexFilter);
+    expect(res).toEqual([message1]);
+  });
+
+  it('should filter a message by a value and header', async () => {
+    const regexFilter = 'request-id-1';
+    const message1 = { headers: {
+      'key1': 'value1',
+    },
+    timestamp: 'stamp',
+    value: 'request-id-1',
+    } as ConsumedMessage;
+
+    const message2 = { headers: {
+      'key1': 'value1', 'x-internal-request-id': 'request-id-1',
+    },
+    timestamp: 'stamp2',
+    value: 'message2',
+    } as ConsumedMessage;
+
+    const consumedMessages = [message1, message2];
+    const res = filterMessages(consumedMessages, regexFilter);
+    expect(res).toEqual([message1, message2]);
+  });
+
 });
