c6bfc2f2c20afb32d0f56f13e5964ed57a66f2c1
Gilad Gur Andelman <gilad@loadmill.com>
2025-12-28 11:36:27 +0200

    🪵🎚️🏷️ Log Level Label

--- src/log/pino-pretty-logger.ts
+++ src/log/pino-pretty-logger.ts
@@ -1,46 +1,50 @@
+import fs from 'fs';
+
 import pino from 'pino';
 
 import { APP_NAME } from '../constants';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const streams: pino.StreamEntry[] = [];
 
-const devLoggerConfig = {
-  level: 'debug',
-  options: {
-    colorize: true,
-    ignore: 'pid,hostname',
-    translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l',
+const addConsoleStream = () => {
+  if (process.env.NODE_ENV === 'development') {
+    const prettyTransport = pino.transport({
+      options: {
+        colorize: true,
+        ignore: 'pid,hostname',
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l',
+      },
+      target: 'pino-pretty',
+    });
+    streams.push({ level: 'debug', stream: prettyTransport });
+  } else {
+    streams.push({ level: 'info', stream: process.stdout });
+  }
+};
+const addFileStream = () => {
+  streams.push({
+    level: process.env.LOG_LEVEL === 'debug' ? 'debug' : 'info',
+    stream: fs.createWriteStream(`${APP_NAME}.log`, { flags: 'a' }),
+  });
+};
+
+addConsoleStream();
+addFileStream();
+
+const pinoLogger = pino(
+  {
+    formatters: {
+      level: (label) => {
+        return { level: label };
+      },
+    },
+    level: 'debug', // Must set base level to debug https://github.com/pinojs/pino/issues/1639#issuecomment-1418324692
   },
-  target: 'pino-pretty',
-};
-
-const fileLoggerConfig = {
-  level: process.env.LOG_LEVEL || 'info',
-  options: {},
-  target: 'pino/file',
-};
-
-const stdoutTarget = isDevelopment ? devLoggerConfig : fileLoggerConfig;
-
-const fileTarget = {
-  level: isDevelopment ? 'debug' : process.env.LOG_LEVEL || 'info',
-  options: {
-    destination: `${APP_NAME}.log`,
-  },
-  target: 'pino/file',
-};
-
-const targets = [
-  fileTarget,
-  stdoutTarget,
-];
-
-const transport = pino.transport({
-  targets,
-});
-
-const pinoLogger = pino({
-  level: 'debug', // https://github.com/pinojs/pino/issues/1639#issuecomment-1418324692
-}, transport);
+  pino.multistream(streams, { dedupe: true }),
+);
 
 export { pinoLogger };
+
+setTimeout(() => {
+  pinoLogger.debug('Pino is DEBUG enabled');
+}, 1);
